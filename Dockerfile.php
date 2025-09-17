4213dcdd2e51dbef82e056a16f3351b32c102011
Alexander Y <startyourday60@gmail.com>
2025-09-17 08:54:13 +0000

    fix: fix docker logic

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,5 +1,5 @@
 FROM php:8.2-apache
 
 COPY apache-custom.conf /etc/apache2/sites-available/000-default.conf
-
+RUN apt-get update && apt-get install -y socat
 RUN mkdir -p /var/log/apache2 && chown -R www-data:www-data /var/log/apache2 && chown www-data:www-data -R /var/www/html
